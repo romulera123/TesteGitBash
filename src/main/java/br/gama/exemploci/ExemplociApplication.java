cd4aa81e1b9f22d57027fe84188b1a0c0024595c
Romulera <romuloaugusto227@gmail.com>
2023-05-16 21:42:24 -0300

    ll

---
+++ src/main/java/br/gama/exemploci/ExemplociApplication.java
@@ -0,0 +1,13 @@
+package br.gama.exemploci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplociApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplociApplication.class, args);
+	}
+
+}
